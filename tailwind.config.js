a07b4e54c95ba99df4b8865610a791556bc76589
hari0082 <2430680@rts-365.dk>
2023-11-23 17:16:36 +0100

    Add input, tailwind and first design

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+  // ...andre konfigurationer...
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+    "./src/App.css", // Tilføj stien til din stilfil her
+  ],
+};
